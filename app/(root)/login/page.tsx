84b02c3aed109e3845e203454d2ef4152375b57e
ITZBaburam <139737597+Baburam99@users.noreply.github.com>
2024-11-28 12:38:46 +0545

    ..

--- app/(root)/login/page.tsx
+++ app/(root)/login/page.tsx
@@ -3,6 +3,7 @@ import { signIn } from '@/auth';
 import { Button } from '@/components/ui/button';
 import { GitHubLogoIcon } from '@radix-ui/react-icons';
 import { redirect } from 'next/navigation';
+import { client } from '@/sanity/lib/client';
 
 
 const page = () => {
@@ -14,7 +15,7 @@ const page = () => {
      
         <div className='border-4 rounded-lg p-2 py-7 flex flex-col gap-9 justify-center items-center border-black shadow-200  translate-y-48'>
         <div className='flex justify-center items-center'>
-            <h1 className='sm:text-26-semibold  text-20-medium uppercase'>Begin Your journey With US</h1>
+            <h1 className='sm:text-26-semibold  text-20-medium uppercase'>Begin Your journey With US! </h1>
           </div>
           <div className='flex flex-col gap-2.5'>
           <form action={async () => {
@@ -22,9 +23,7 @@ const page = () => {
               await signIn('github');
             
           } }>
-              <Button onClick={() => {
-                  redirect('/')
-          }} className='sm:text-2xl  text-white shadow-200 uppercase '>Sign In with GITHUB <span className='px-0.5'><GitHubLogoIcon/></span> </Button>
+              <Button type='submit' className='sm:text-2xl  text-white shadow-200 uppercase '>Sign In with GITHUB <span className='px-0.5'><GitHubLogoIcon/></span> </Button>
           </form>
          
          
